7d9c1b2f148688647f8b8ae8edda1b9a6dd7e4f8
MesayBeyene <mesibella@gmail.com>
2020-08-28 15:05:47 -0700

    db connected

--- server.js
+++ server.js
@@ -4,12 +4,32 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 //const cookieParser = require("cookie-parser");
 
+//Creating .env file location.
+dotenv.config({ path: "./.env" });
+
 const app = express();
 const port = 8080;
 
 //Creating public directory(where we can save public files)...__dirname gives the current directory
 const publicDirectory = path.join(__dirname, "./public");
 app.use(express.static(publicDirectory));
+
+//Creating environmental .env DB Connection
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
+});
+
+//Connecting DB
+db.connect((error) => {
+  if (error) {
+    console.log("Error");
+  } else {
+    console.log("MySQLDB Connected");
+  }
+});
 
 //Connecting HTML pages
 app.set("view engine", "hbs");
