ceb1d53d21a0beaaf0dda058fdcb0557f8c0039a
MesayBeyene <mesibella@gmail.com>
2020-08-28 00:00:48 -0700

    fixing

--- server.js
+++ server.js
@@ -1,28 +1,37 @@
-//Import
-var express = require("express");
-var app = express();
+const express = require("express");
+const path = require("path"); //no need to install it came from node js
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+//const cookieParser = require("cookie-parser");
+
+const app = express();
 const port = 8080;
 
-//Static files
-app.use(express.static("public"));
-app.use("/css", express.static(__dirname + "public/css"));
-app.use("/js", express.static(__dirname + "public/js"));
-app.use("/img", express.static(__dirname + "public/img"));
+//Creating public directory(where we can save public files)...__dirname gives the current directory
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory));
 
-app.get("", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+//Connecting HTML pages
+app.set("view engine", "hbs");
+
+//Connecting index.hbs
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
+//Connecting assignmentLog.hbs
 app.get("/assignmentLog", (req, res) => {
-  res.sendFile(__dirname + "/views/assignmentLog.html");
+  res.render("assignmentLog");
 });
 
+//Connecting developerLog.hbs
 app.get("/developerLog", (req, res) => {
-  res.sendFile(__dirname + "/views/developerLog.html");
+  res.render("developerLog");
 });
 
+//Connecting generalReport.hbs
 app.get("/generalReport", (req, res) => {
-  res.sendFile(__dirname + "/views/generalReport.html");
+  res.render("generalReport");
 });
 
 //Listen on port 8080
